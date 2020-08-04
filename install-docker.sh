a0414998361752ad85e0613dce5376b96aa5ac61
Jiangqi Li <jiangqi319@gmail.com>
2020-08-03 23:00:18 -0400

    init

---
+++ install-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "installing docker"
+echo "deleting old version docker"
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+echo "install containerd package"
+sudo yum install -y https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+echo "install docker-ce"
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+echo "start docker service"
+sudo systemctl start docker
